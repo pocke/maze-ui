859066d7742b98d7d34b3c64e8067217e2f99faf
Masataka Kuwabara <p.ck.t22@gmail.com>
2016-07-20 22:29:29 +0900

    Show navbar

--- src/index.js
+++ src/index.js
@@ -2,7 +2,24 @@ import 'babel-polyfill';
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+class Header extends React.Component {
+  render() {
+    return (
+      <header className="navbar navbar-default navbar-fixed-top">
+        <p className="navbar-brand">Maze</p>
+      </header>
+    );
+  }
+}
+
 ReactDOM.render(
-  <h1>Hello!</h1>,
+  (
+    <div>
+      <Header></Header>
+      <div className="container" style={{paddingTop: '60px'}}>
+        <h1>Hello!</h1>
+      </div>
+    </div>
+  ),
   document.getElementById('react-main')
 );
