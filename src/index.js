1be6a0cd0cb90c2c10475e73d0918e0cef3593a6
Masataka Kuwabara <p.ck.t22@gmail.com>
2016-07-22 14:40:48 +0900

    Show posts

--- src/index.js
+++ src/index.js
@@ -5,6 +5,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, Link, browserHistory } from 'react-router';
 
+
+function fetchch(path, option) {
+  const DefaultFetchOption = {
+  };
+  const URLBase = 'http://localhost:3000';
+
+  const opt = Object.assign({}, DefaultFetchOption, option);
+  const url = `${URLBase}${path}`;
+  return fetch(url, opt);
+}
+
 class Header extends React.Component {
   render() {
     return (
@@ -16,9 +27,35 @@ class Header extends React.Component {
 }
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+    };
+
+    (async () => {
+      const resp = await fetchch('/posts', {});
+      const posts = await resp.json();
+      this.setState({
+        posts: posts,
+      });
+      console.log(this.state.posts);
+    })();
+  }
+
   render() {
+    console.log(this.state.posts);
     return (
-      <h1>Hello React!</h1>
+      <div>
+        <h2>News</h2>
+        <ul>
+          {
+            this.state.posts.map((post) => {
+              return <li>{post.value}</li>
+            })
+          }
+        </ul>
+      </div>
     )
   }
 }
